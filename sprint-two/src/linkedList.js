bb62f57a8a2a6b631bb598000d9aebc527e8b850
Dominic Czarnota <dczarno@gmail.com>
2014-07-27 16:45:00 -0700

    Finished all objectives for linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,46 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var node = makeNode(value);
+
+    if(list.head === null){
+      list.head = node;
+      list.tail = node;
+    }
+    
+    list.head.next = node;
+    list.tail = node;
+
   };
 
   list.removeHead = function(){
+    var node = this;
+    var removedHead = this.head.value;
+
+    node.head = node.head.next;
+    return removedHead;
   };
 
   list.contains = function(target){
+    var node = this.head;
+    var result = false;
+
+    var containsNode = function(target, node, result){
+      if(node.value === target){
+        result = true;
+        return result;
+      }
+
+      node = node.next;
+      if(node){
+        return containsNode(target, node, result);
+      }
+
+      return result;
+    };
+
+    return containsNode(target, node, result);
+
   };
 
   return list;

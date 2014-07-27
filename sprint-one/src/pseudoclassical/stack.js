4c4f9aa3b30054fcfc57fcda17760de9842463c6
Dominic Czarnota <dczarno@gmail.com>
2014-07-27 15:19:08 -0700

    Finished Pseudoclassical (stack)

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,27 @@
+//PSEUDOCLASSICAL PATTERN
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.currentsize = 0;
+  this.storage = {};
 };
 
+Stack.prototype.push = function(value){
+	this.storage[this.currentsize] = value;
+	this.currentsize++;
+};
 
+Stack.prototype.pop = function(){
+	if(this.currentsize>0){
+		this.currentsize--;
+	}
+
+	var result = this.storage[this.currentsize];
+	delete this.storage[this.currentsize];
+
+	return result;
+};
+
+Stack.prototype.size = function(){
+	return this.currentsize;
+};

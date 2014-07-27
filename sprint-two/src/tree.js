b0f2171d3d484b058822f9e02684fb9d67a381f7
Dominic Czarnota <dczarno@gmail.com>
2014-07-27 15:21:09 -0700

    Finished all objectives for tree data structures

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,7 +1,8 @@
 var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
-  newTree.children = undefined;
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
@@ -11,13 +12,31 @@ var makeTree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+	var tree = makeTree(value);
+	this.children.push(tree);
 };
 
 treeMethods.contains = function(target){
+	var node = this;
+	var result = false;
+
+	var checkNode = function(node, target){
+		if(node.value === target){
+			result = true;
+			return result;
+		}
+		if(node.children.length > 0){
+			for (var i = 0; i < node.children.length; i++) {
+				checkNode(node.children[i], target);
+			}
+		}
+    	return result;
+	};
+
+	checkNode(node, target);
+	return result;
 
 };
-
 
 /*
  * Complexity: What is the time complexity of the above functions?

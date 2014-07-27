cbb00d01ad457ccb08d47254379d2f8ef1d7d0e0
Dominic Czarnota <dczarno@gmail.com>
2014-07-27 15:17:25 -0700

    Finished Functional (queue)

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,32 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
+  var front = 0;
+  var index = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[index] = value;
+    size++;
+    index++;
   };
 
   someInstance.dequeue = function(){
+    if(size>0){
+      size--;
+    }
+
+    var result = storage[front];
+    delete storage[front];
+    front++;
+    
+    return result;
   };
 
   someInstance.size = function(){
+    return size;
   };
 
   return someInstance;

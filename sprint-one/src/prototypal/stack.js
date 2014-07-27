3da6542916fd1bbef29cc60dec4ad5818216164c
Dominic Czarnota <dczarno@gmail.com>
2014-07-27 15:18:14 -0700

    Finished Prototypal (stack)

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,28 @@
 var makeStack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance.storage = {};
+  someInstance.currrentsize = 0;
+
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+	push: function(value){
+		this.storage[this.currrentsize] = value;
+		this.currrentsize++;
+	},
 
+	pop: function(){
+		if(this.currrentsize>0){
+			this.currrentsize--;
+		}
 
+		return this.storage[this.currrentsize];
+	},
+
+	size: function(){
+		return this.currrentsize;
+	}
+};

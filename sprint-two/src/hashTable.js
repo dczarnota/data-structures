c71f30b18fa5f6e23d9baf6567d5a07bb58e53c0
Dominic Czarnota <dczarno@gmail.com>
2014-07-27 18:01:03 -0700

    Finished all objectives for hash tables

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,64 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+
+  var bucket = [];
+
+  if(this._storage.get(i) === undefined){
+  	bucket.push([k ,v]);
+  	this._storage.set(i, bucket);
+  }
+
+  bucket = this._storage.get(i);
+  var foundTuple = false;
+  for(var j=0; j<bucket.length; j++){
+  	var tuple = bucket[j]
+  	if(tuple[0] === k){
+  		tuple = [k ,v];
+  		foundTuple = true;
+  		break;
+  	}
+  }
+
+  if(!foundTuple){
+  	bucket.push([k ,v]);	
+  }
+  
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
 
+  var bucket = this._storage.get(i);
+
+  if(!bucket){
+  	return null;
+  }
+
+  for(var j=0; j<bucket.length; j++){
+  	if(bucket[j][0] === k){
+  		return bucket[j][1];
+  	}
+  }
+
+  return null;
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
 
+  var bucket = this._storage.get(i);
+  if(!bucket){
+  	return null;
+  }
+
+  for(var j=0; j<bucket.length; j++){
+  	var tuple = bucket[j];
+  	if(tuple[0] === k){
+  		bucket.splice(j);
+  	}
+  }
+  return null;
 };
 
 
